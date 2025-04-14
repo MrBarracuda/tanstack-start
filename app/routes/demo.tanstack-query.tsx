84c62c52c08abf79ec053582ac89a6bdf135b02a
MrBarracuda <1dima9999@gmail.com>
2025-04-14 18:27:00 +0200

    setup shadcn

---
+++ app/routes/demo.tanstack-query.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/demo/tanstack-query')({
+  component: TanStackQueryDemo,
+});
+
+function TanStackQueryDemo() {
+  const { data } = useQuery({
+    queryKey: ['people'],
+    queryFn: () =>
+      fetch('https://swapi.dev/api/people')
+        .then((res) => res.json())
+        .then((d) => d.results as { name: string }[]),
+    initialData: [],
+  });
+
+  return (
+    <div className="p-4">
+      <h1 className="mb-4 text-2xl">People list from Swapi</h1>
+      <ul>
+        {data.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
